1b3a51d9e58394c653946e6ea6872b5e393ab0b4
Yakumwamba <brianlemba2@gmail.com>
2024-01-27 01:53:39 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodflamegame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:very_good_flame_game/app/app.dart';
+import 'package:very_good_flame_game/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
